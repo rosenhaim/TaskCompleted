3d9f3e93f51c7d6923b96f74946001f709d80e9e
rosenhaim <rosenhaim@gmail.com>
2023-08-02 21:54:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.taskcompleted
+
+import android.graphics.fonts.FontStyle
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskcompleted.ui.theme.TaskCompletedTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskCompletedTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskCompletedImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+            {
+                Image(
+                painter = image,
+                contentDescription = null,
+
+                )
+                TaskCompletedText()
+            }
+
+}
+
+@Composable
+fun TaskCompletedText(){
+    Text(
+        text = stringResource(R.string.tasks_completed),
+        textAlign = TextAlign.Center,
+        modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+        fontWeight = FontWeight.Bold
+        )
+    Text(
+        text = stringResource(R.string.nice_work),
+        textAlign = TextAlign.Center,
+        fontSize = 16.sp
+        )
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TaskCompletedPreview() {
+    TaskCompletedTheme {
+        TaskCompletedImage()
+    }
+}
